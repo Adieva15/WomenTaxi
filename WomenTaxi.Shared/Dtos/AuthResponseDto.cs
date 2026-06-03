72dd15eb13aa417796167e63b51bf33ce7c00b88
Adieva15 <adievadine@gmail.com>
2026-06-03 19:32:03 +0300

    dto ответ после логина/регистрации

--- WomenTaxi.Shared/Dtos/AuthResponseDto.cs
+++ WomenTaxi.Shared/Dtos/AuthResponseDto.cs
@@ -8,5 +8,13 @@ namespace WomenTaxi.Shared.Dtos
 {
     class AuthResponseDto
     {
+        //ответ после логина/регистрации
+        public string Token { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public String Role { get; set; } = string.Empty;
+
+        public string FirstName { get; set; } = string.Empty;
     }
 }
--- WomenTaxi.Shared/Dtos/LoginDto.cs
+++ WomenTaxi.Shared/Dtos/LoginDto.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WomenTaxi.Shared.Dtos
 {
-    class LoginDto
+    public class LoginDto
     {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
     }
 }
