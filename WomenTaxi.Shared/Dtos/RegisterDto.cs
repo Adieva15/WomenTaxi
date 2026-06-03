54c9109047c0668ebb49787cf7838cb402df406b
Adieva15 <adievadine@gmail.com>
2026-06-03 19:17:22 +0300

    создание DTO для регистрации

---
+++ WomenTaxi.Shared/Dtos/RegisterDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WomenTaxi.Shared.Dtos
+{
+    //Создание DTO для регистрации и логина
+    public class RegisterDto
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required, MinLength(6)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required, Phone]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+        public string Role { get; set; } = "Passenger";// Passenger, Driver
+    }
+}
