3b131f9ff90ef562407c230d3af149fa3b3d125c
Adieva15 <adievadine@gmail.com>
2026-06-03 22:24:58 +0300

    настройка аутентификации

--- WomenTaxi/WomenTaxi/Program.cs
+++ WomenTaxi/WomenTaxi/Program.cs
@@ -2,12 +2,38 @@ using WomenTaxi.Client.Pages;
 using WomenTaxi.Components;
 using Microsoft.EntityFrameworkCore;
 using WomenTaxi.Data;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using WomenTaxi.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Добавляем DbContext с PostgreSQL
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+//Настройка аутентификации
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"] ?? throw new InvalidOperationException("JWT Key missing")))
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 
 // Add services to the container, +(DbContext, SignalR, авторизация и т.д.)
@@ -17,11 +43,12 @@ builder.Services.AddRazorComponents()
 //если ваш сервер сам будет ходить в сторонние API (Яндекс.Карты, 2ГИС, платежи). Без этого не получите расстояние и стоимость поездки
 builder.Services.AddHttpClient();// Регистрируем фабрику HTTP-клиентов
 
-
-
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
 //настройка middleware(UseRouting, UseEndpoints и т.д.)
+
 
 // Configure the HTTP request pipeline, Настройка конвейера middleware
 if (app.Environment.IsDevelopment())
